f4bef2c3f5e3e0a990effa0acac1d95a65f362e6
Evan Rosen <Evan@admins-MacBook-Pro.local>
2019-09-16 10:16:41 -0700

    completed reverse number problem

--- sept16.js
+++ sept16.js
@@ -2,14 +2,48 @@
 
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-function twoSum(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[j] === target - nums[i]) {
-        return [i, j];
-      }
-    }
-  }
-}
+// function twoSum(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[j] === target - nums[i]) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// }
 
-twoSum([2, 7, 11, 15], 9);
+// twoSum([2, 7, 11, 15], 9);
+
+// Given a 32-bit signed integer, reverse digits of an integer.
+
+// Example 1:
+
+// Input: 123
+// Output: 321
+// Example 2:
+
+// Input: -123
+// Output: -321
+// Example 3:
+
+// Input: 120
+// Output: 21
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+  let reverseNum =
+    parseInt(
+      x
+        .toString()
+        .split("")
+        .reverse()
+        .join("")
+    ) * Math.sign(x);
+  console.log(reverseNum);
+  if (reverseNum < Math.pow(2, 31) * -1 || reverseNum > Math.pow(2, 31) - 1) {
+    return 0;
+  } else return reverseNum;
+};
